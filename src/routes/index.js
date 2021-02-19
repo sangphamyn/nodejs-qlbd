9b826ddd284bd8417131b710a48b5fa2b82223c8
Sang <sangphamyn313@gmail.com>
2021-02-19 18:14:25 +0700

    add schedule

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const allFootballClub = require('./allFootballClub');
 const footballClub = require('./footballClubs');
 const ttRouter = require('./tt');
 const homeRouter = require('./home');
+const scheduleRouter = require('./schedule');
 
 function route(app){
     app.use('/create', createRouter);
     app.use('/all-fb-club', allFootballClub);
     app.use('/all-fb-club', footballClub);
     app.use('/tt', ttRouter);
+    app.use('/schedule', scheduleRouter);
     app.use('/', homeRouter);
 }
 
